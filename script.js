394f6ec1e1440b290e9687ff8d8ab4bd271ffa45
Angela J Holden <angelajholden@gmail.com>
2025-10-13 20:06:37 -0500

    styles for header, nav, hero and image grid

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const body = document.querySelector("body");
+	const buttons = document.querySelectorAll(".menu_button");
+	const open = document.querySelector(".open_button");
+
+	buttons.forEach((button) => {
+		button.addEventListener("click", () => {
+			const isActive = body.classList.toggle("menu_active");
+			if (isActive) {
+				open.setAttribute("aria-expanded", "true");
+			} else {
+				open.setAttribute("aria-expanded", "false");
+			}
+		});
+	});
+
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Escape" && body.classList.contains("menu_active")) {
+			body.classList.remove("menu_active");
+			open.setAttribute("aria-expanded", "false");
+		}
+	});
+});
